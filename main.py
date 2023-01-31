30a92f279085fd0f501c162e4a659db4cabea598
Anargyros Tsadimas <tsadimas@hua.gr>
2023-01-31 17:06:09 +0200

    added flask-bootrtrap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, url_for, request, render_template
+from flask_bootstrap import Bootstrap
 
 import os
 from dotenv import load_dotenv
@@ -16,6 +17,7 @@ else:
 
 
 app = Flask(__name__)
+Bootstrap(app)
 
 app.logger.info('Environmental variables Initialized')
 
